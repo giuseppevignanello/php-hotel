33567ca480d71634b5360c707ce79514522b651d
giuseppevignanello <peppe.vignanello@gmail.com>
2023-05-04 15:39:15 +0200

    add data in a table

--- index.php
+++ index.php
@@ -41,8 +41,80 @@ $hotels = [
 ];
 
 
-foreach ($hotels as $hotel) {
-    var_dump($hotel);
-}
-
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'
+        integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
+    <title>Hotels</title>
+</head>
+
+<body>
+
+
+    <table class="table">
+
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">
+                    Name
+                </th>
+                <th scope="col">
+                    Description
+                </th>
+                <th scope="col">
+                    Parking
+                </th>
+                <th scope="col">
+                    Vote
+                </th>
+                <th scope="col">
+                    Distance to Center
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <?php
+                foreach ($hotels as $hotel) {
+                    ?>
+
+                    <th scope="row">
+
+                    </th>
+                    <td>
+                        <?= $hotel["name"] ?>
+                    </td>
+                    <td>
+                        <?= $hotel["description"] ?>
+                    </td>
+                    <td>
+                        <?php if ($hotel["parking"] === true) { ?>
+                            ✅
+                        <?php } else { ?>
+                            ❌
+                            <?php
+                        } ?>
+                    </td>
+                    <td>
+                        <?= $hotel["vote"] ?>
+                    </td>
+                    <td>
+                        <?= $hotel["distance_to_center"] ?>
+                    </td>
+                </tr>
+            </tbody>
+        <?php }
+                ?>
+    </table>
+</body>
+
+</html>
